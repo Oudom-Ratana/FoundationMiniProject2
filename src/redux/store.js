1e50ebb93b7ee424ce3d8c25f3280b269c8e7e67
Oudom-Ratana <ratanaoudom997@gmail.com>
2026-08-09 11:01:56 +0700

    fix error

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { counterSlice } from "../features/counter/counterSlice";
-import { cartSlice } from "../features/cart/cartSlice";
-import { authSlice } from "../features/auth/authSlice";
+import counterReducer from "../features/counter/counterSlice";
+import cartReducer from "../features/cart/cartSlice";
+import authReducer from "../features/auth/authSlice";
 import { baseApi } from "../services/baseApi";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
-      counter: counterSlice.reducer,
-      cart: cartSlice.reducer,
-      auth: authSlice.reducer,
+      counter: counterReducer,
+      cart: cartReducer,
+      auth: authReducer,
       [baseApi.reducerPath]: baseApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
